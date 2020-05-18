79ea3106f16dd22f9b007392b73c96c6afd15bf9
Zana H <zhertica@gmail.com>
2020-05-18 02:30:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   index = 0
   while array[index] do 
-    new_array.push(Math.sqrt(array[index))
+    new_array.push(Math.sqrt(array[index])
     index += 1
   end  
   new_array
